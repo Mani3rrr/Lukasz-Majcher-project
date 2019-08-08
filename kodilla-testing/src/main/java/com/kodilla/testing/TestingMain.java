96a741435325d9ed15056bebdca1e87321199726
Mani3rrr <lukaszmajcher1@gmail.com>
2019-08-08 16:32:27 +0200

    Second Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,4 +4,4 @@ public class TestingMain {
     public static void main(String[] args){
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
     }
-}
+}git remote add origin
