5eb9afc1603d16e208f87a41a56f78664e4a39a0
Mani3rrr <lukaszmajcher1@gmail.com>
2019-08-17 22:54:27 +0200

    Commit12

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -5,18 +5,17 @@ import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
         Forum forum = new Forum();
-        LocalDate date = LocalDate.of(1999,8,17);
+
 
         Map<Integer, ForumUser> theResultMapOfUsers = forum.getUserList().stream()
                 .filter(forumuser -> forumuser.getUserGender()== 'M')
-                .filter(forumuser -> forumuser.getUserBirth().isBefore(date))
+                .filter(forumuser -> forumuser.getUserBirth().isBefore(LocalDate.now().minusYears(20)))
                 .filter(forumuser -> forumuser.getUserPosts() > 0)
                 .collect(Collectors.toMap(ForumUser::getUserId, forumuser -> forumuser));
 
@@ -24,5 +23,7 @@ public class StreamMain {
         theResultMapOfUsers.entrySet().stream()
                 .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
+
+
     }
 }
